3b6121d5fc1805998f51a74bb7df865760a09a42
dain russell <dain.russell@gmail.com>
2018-10-30 22:11:39 -0400

    starter news app with Java class and adapter

--- app/src/main/java/com/example/dainr/project6newsapp1/News.java
+++ app/src/main/java/com/example/dainr/project6newsapp1/News.java
@@ -3,23 +3,36 @@ package com.example.dainr.project6newsapp1;
 public class News {
 
     //class declaration with 4 instance variables for this fragment
+
+    //section name
+    private final String articleSection;
+
+    //article name
     private final String articleName;
 
+    //article's author
     private final String articleAuthor;
 
+    //article's date
     private final String articleDate;
 
+    //article's Url
     private final String articleUrl;
 
     // constructors in which we instantiate the instance variables
-    News(String articleName, String articleAuthor, String articleDate, String articleUrl){
+    News(String articleSection,String articleName, String articleAuthor, String articleDate, String articleUrl){
+        this.articleSection= articleSection;
         this.articleName= articleName;
         this.articleAuthor=articleAuthor;
         this.articleDate=articleDate;
         this.articleUrl=articleUrl;
     }
 
-    // get methods that return Strings and an integer
+    // get methods that return Strings
+    String getArticleSection(){
+        return articleSection;
+    }
+
     String getArticleName(){
         return articleName;
     }
@@ -35,7 +48,5 @@ public class News {
     String getArticleUrl(){
         return articleUrl;
     }
-
-
 }
 
--- app/src/main/java/com/example/dainr/project6newsapp1/NewsActivity.java
+++ app/src/main/java/com/example/dainr/project6newsapp1/NewsActivity.java
@@ -1,6 +1,5 @@
 package com.example.dainr.project6newsapp1;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
@@ -11,36 +10,36 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class NewsAdapter extends ArrayAdapter<News> {
+public class NewsActivity extends ArrayAdapter<News> {
 
-    NewsAdapter(Activity context, ArrayList<News> news) {
+    public NewsActivity(Context context, ArrayList<News> news) {
         super(context, 0, news);
 
     }
-
     @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         View listItemView  = convertView;
         if (listItemView == null) {
             //The inflater is used to map the instance variables to the TextView in the
-            listItemView= LayoutInflater.from(getContext()).inflate(R.layout.main_news_list_view, parent, false);
+            listItemView= LayoutInflater.from(getContext()).inflate(R.layout.card_view_display, parent, false);
         }
+
+        // get object located at the position
+       News currentNews= getItem(position);
 
         //The different objects at each position located by each of their Id's
 
+
+        assert convertView != null;
         TextView sectionView = listItemView.findViewById(R.id.article_section);
         TextView nameView = listItemView.findViewById(R.id.article_name);
         TextView authorView = listItemView.findViewById(R.id.article_author);
-        TextView dateView = listItemView.findViewById(R.id.article_date);
+        TextView dateView= listItemView.findViewById(R.id.article_date);
 
-        // get object located at the position
-        News currentNews= getItem(position);
-
-
-
+        assert currentNews != null;
         sectionView.setText(currentNews.getArticleSection());
-        nameView.setText(currentNews.getArticleSection());
+        nameView.setText(currentNews.getArticleName());
         authorView.setText(currentNews.getArticleAuthor());
         dateView.setText(currentNews.getArticleDate());
 
